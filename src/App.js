0752c48907e7ec294a90287ffed77ec921369d34
Koua Thao <kouathao09@gmail.com>
2019-12-01 13:36:24 -0600

    add rental detail and rental listing page, add bootstrap scss, add redux and routing

--- src/App.js
+++ src/App.js
@@ -1,19 +1,39 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
+
+// imported components
+// 1) Provider redux is a component and must wrap around all component
+import { Provider } from "react-redux";
+import { Header } from "./shared/Header";
+import RentalListing from "./components/rental/rental-listing/RentalListing";
+import RentalDetail from "./components/rental/rental-detail/RentalDetail";
+
+// store
+const store = require("./reducers").init();
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Header />
+            <div className="container">
+              {/* render = redirect  */}
+              <Route
+                exact
+                path="/"
+                render={() => {
+                  return <Redirect to="/rentals" />;
+                }}
+              />
+              <Route exact path="/rentals" component={RentalListing} />
+              <Route exact path="/rentals/:id" component={RentalDetail} />
+            </div>
+          </div>
+        </Router>
+      </Provider>
     );
   }
 }
---
+++ src/reducers/rental-reducer.js
@@ -0,0 +1,34 @@
+import { FETCH_RENTALS, FETCH_RENTAL_BY_ID_SUCCESS, FETCH_RENTAL_BY_ID_INIT } from "../actions/types";
+
+// initial state
+const INITIAL_STATE = {
+  rentals: {
+    data: []
+  },
+  rental: {
+    data: {}
+  }
+};
+
+// 1) export
+// 2) function will recieve a state and an action
+
+export const rentalReducer = (state = INITIAL_STATE.rentals, action) => {
+  switch (action.type) {
+    case FETCH_RENTALS:
+      return { ...state, data: action.rentals };
+    default:
+      return state;
+  }
+};
+
+export const selectedRentalReducer = (state = INITIAL_STATE.rental, action) => {
+  switch (action.type) {
+    case FETCH_RENTAL_BY_ID_INIT:
+      return {...state, data: {}}
+    case FETCH_RENTAL_BY_ID_SUCCESS:
+      return { ...state, data: action.rental };
+    default:
+      return state;
+  }
+};
